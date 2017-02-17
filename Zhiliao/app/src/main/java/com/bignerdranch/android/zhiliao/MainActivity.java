fec5464d67d20f00cae67c75d89e56bc10244371
leebanon <leebanon@gmail.com>
2017-02-17 14:46:49 +0800

    Gradle build finished, but fail to install

--- Zhiliao/app/src/main/java/com/bignerdranch/android/zhiliao/MainActivity.java
+++ Zhiliao/app/src/main/java/com/bignerdranch/android/zhiliao/MainActivity.java
@@ -2,12 +2,35 @@ package com.bignerdranch.android.zhiliao;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
+
+import io.rong.imkit.RongIM;
+import io.rong.imlib.RongIMClient;
 
 public class MainActivity extends Activity {
+
+    private static final String token1 = "MSj5ApTgpvwrQoupM2R3sfhJ+LSWphCHE+/oC0b/YHPmWlr46cLgMcZyHvsx4hdQgeWbUuSOIBZ4FcFDMDKcvQ==";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        RongIM.connect(token1, new RongIMClient.ConnectCallback() {
+            @Override
+            public void onTokenIncorrect() {
+
+            }
+
+            @Override
+            public void onSuccess(String s) {
+                Log.e("onSuccess","onSuccess, userId: "+s);
+            }
+
+            @Override
+            public void onError(RongIMClient.ErrorCode errorCode) {
+                Log.e("onError","onError, errorCode: "+errorCode.getValue());
+            }
+        });
     }
 }
